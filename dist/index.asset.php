9dbfffd94249cb94ac46d976c402e27c27d3c851
Monir <fencemonir@gmail.com>
2022-10-01 23:36:31 +0600

    update build and readme

--- dist/index.asset.php
+++ dist/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '85f3c3d88ebf67893243');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'fa5657e0a07b9b69a27f');
